a241182610857ea7b95d8c78e903eadda74cdc77
Richard Shea <rshea@thecubagroup.com>
2016-10-03 12:59:25 +1300

    Fleshing out game classes and initial main()

--- kotahi/core.py
+++ kotahi/core.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+
+from helpers import Game, GameStyle
+
+
+def main():
+    g = Game(GameStyle.monopoly, 99)
+
+if __name__ == "__main__":
+    main()
--- kotahi/helpers.py
+++ kotahi/helpers.py
@@ -1,12 +1,42 @@
+from enum import Enum
+
+
+class GameStyle(Enum):
+    monopoly = 1
+    scrabble = 2
+
+
+class MonopolyPieceStyle(Enum):
+    dog = 1
+    sheep = 2
+    car = 3
+    mobilephone = 4
+
+
 class Game:
     '''Represents the game being played'''
-    def __init__(self):
+    def __init__(self, gs, cntPlayers,):
+        if gs != GameStyle.monopoly:
+            raise NotImplementedError("Only Monopoloy is supported currently")
+        if cntPlayers > len(MonopolyPieceStyle):
+            raise NotImplementedError("Two many players for the number of available pieces")
+
+
+class Board:
+    '''Represents the board of the game being played'''
+    def __init__(self, ):
         pass
 
 
 class Player:
     '''Represents a player of the game'''
     def __init__(self):
+        pass
+
+
+class Players:
+    '''Represents a player of the game'''
+    def __init__(self, cnt):
         pass
 
 
@@ -18,9 +48,18 @@ class Place:
 
 class Square(Place):
     '''
-    Represents a position of for a game
+    Represents a position for a game
     where positions are 'squares'
     (such as Monopoly)
+    '''
+    def __init__(self):
+        pass
+
+
+class Piece():
+    '''
+    Represents the token belonging
+    to a given `Player`
     '''
     def __init__(self):
         pass
