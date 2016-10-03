eefa9ccea61aabf378d4768863ee99b29188a453
Richard Shea <rshea@thecubagroup.com>
2016-10-03 23:55:06 +1300

    Improve classes. Provide startup data in comments - half done

--- kotahi/core.py
+++ kotahi/core.py
@@ -4,7 +4,7 @@ from helpers import Game, GameStyle
 
 
 def main():
-    g = Game(GameStyle.monopoly, 99)
+    g = Game(GameStyle.monopoly, 4)
 
 if __name__ == "__main__":
     main()
--- kotahi/helpers.py
+++ kotahi/helpers.py
@@ -2,24 +2,149 @@ from enum import Enum
 
 
 class GameStyle(Enum):
-    monopoly = 1
+    monopolyuk = 1
     scrabble = 2
 
 
-class MonopolyPieceStyle(Enum):
-    dog = 1
+class MonopolyPieceStyle(Enum): 
+    dog = 1 
     sheep = 2
     car = 3
     mobilephone = 4
 
 
+class MonopolySquareStyle(Enum):
+    go = 1
+    jail = 2
+    freeparking = 3
+    communitychest = 4
+    chance = 4
+    tax = 5
+    property = 6
+
+
+class MonopolyPropertySquareSide(Enum):
+    first = 1
+    second = 2
+    third = 3
+    fourth= 4
+
+
+class MonopolyPropertyStyle(Enum):
+    brown = 1
+    lightblue = 2
+    pink = 3
+    orange = 4
+    red = 5
+    yellow = 6
+    green = 7
+    darkblue = 8
+    transport = 9
+    utilities = 9
+
+'''
+dicProps = {}
+dicProps['uk'] = []
+dicProps['us'] = []
+dicProps['uk'].append(Square('Old Kent Road', MonopolySquareStyle.property, MonopolyPropertyStyle.brown, MonopolyPropertySquareSide.first))
+dicProps['us'].append(Square('Mediterranean Avenue', MonopolySquareStyle.property, MonopolyPropertyStyle.brown, MonopolyPropertySquareSide.first))
+dicProps['uk'].append(Square('Whitechapel Road', MonopolySquareStyle.property, MonopolyPropertyStyle.brown, MonopolyPropertySquareSide.first))
+dicProps['us'].append(Square('Baltic Avenue', MonopolySquareStyle.property, MonopolyPropertyStyle.brown, MonopolyPropertySquareSide.first))
+dicProps['uk'].append(Square('The Angel Islington', MonopolySquareStyle.property, MonopolyPropertyStyle.lightblue, MonopolyPropertySquareSide.first))
+dicProps['us'].append(Square('Oriental Avenue', MonopolySquareStyle.property, MonopolyPropertyStyle.lightblue, MonopolyPropertySquareSide.first))
+dicProps['uk'].append(Square('Euston Road', MonopolySquareStyle.property, MonopolyPropertyStyle.lightblue, MonopolyPropertySquareSide.first))
+dicProps['us'].append(Square('Vermont Avenue', MonopolySquareStyle.property, MonopolyPropertyStyle.lightblue, MonopolyPropertySquareSide.first))
+dicProps['uk'].append(Square('Pentonville Road', MonopolySquareStyle.property, MonopolyPropertyStyle.lightblue, MonopolyPropertySquareSide.first))
+dicProps['us'].append(Square('Connecticut Avenue', MonopolySquareStyle.property, MonopolyPropertyStyle.lightblue, MonopolyPropertySquareSide.first))
+dicProps['uk'].append(Square('Pentonville Road', MonopolySquareStyle.property, MonopolyPropertyStyle.lightblue, MonopolyPropertySquareSide.first))
+dicProps['us'].append(Square('Connecticut Avenue', MonopolySquareStyle.property, MonopolyPropertyStyle.lightblue, MonopolyPropertySquareSide.first))
+dicProps['uk'].append(Square('Pall Mall', MonopolySquareStyle.property, MonopolyPropertyStyle.pink, MonopolyPropertySquareSide.first))
+dicProps['us'].append(Square('St Charles Place', MonopolySquareStyle.property, MonopolyPropertyStyle.pink, MonopolyPropertySquareSide.first))
+dicProps['uk'].append(Square('Whitehall', MonopolySquareStyle.property, MonopolyPropertyStyle.pink, MonopolyPropertySquareSide.first))
+dicProps['us'].append(Square('States Avenue', MonopolySquareStyle.property, MonopolyPropertyStyle.pink, MonopolyPropertySquareSide.first))
+dicProps['uk'].append(Square('Northumberland Avenue', MonopolySquareStyle.property, MonopolyPropertyStyle.pink, MonopolyPropertySquareSide.first))
+dicProps['us'].append(Square('Virginia Avenue', MonopolySquareStyle.property, MonopolyPropertyStyle.pink, MonopolyPropertySquareSide.first))
+dicProps['uk'].append(Square('Bow Street', MonopolySquareStyle.property, MonopolyPropertyStyle.orange, MonopolyPropertySquareSide.first))
+dicProps['us'].append(Square('St James Place', MonopolySquareStyle.property, MonopolyPropertyStyle.orange, MonopolyPropertySquareSide.first))
+dicProps['uk'].append(Square('Marlborough Street', MonopolySquareStyle.property, MonopolyPropertyStyle.orange, MonopolyPropertySquareSide.first))
+dicProps['us'].append(Square('Tennessee Place', MonopolySquareStyle.property, MonopolyPropertyStyle.orange, MonopolyPropertySquareSide.first))
+dicProps['uk'].append(Square('Vine Street', MonopolySquareStyle.property, MonopolyPropertyStyle.orange, MonopolyPropertySquareSide.first))
+dicProps['us'].append(Square('New York Avenue', MonopolySquareStyle.property, MonopolyPropertyStyle.orange, MonopolyPropertySquareSide.first))
+
+Brown (Dark Purple)
+
+    Old Kent Road/Mediterranean Avenue
+    Whitechapel Road/Baltic Avenue 
+
+Light Blue
+
+    The Angel Islington/Oriental Avenue
+    Euston Road/Vermont Avenue
+    Pentonville Road/Connecticut Avenue 
+
+Pink
+
+    Pall Mall/St. Charles Place
+    Whitehall/States Avenue
+    Northumberland Avenue/Virginia Avenue 
+
+Orange
+
+    Bow Street/St. James Place
+    Marlborough Street/Tennessee Avenue
+    Vine Street/New York Avenue 
+
+Red
+
+    The Strand/Kentucky Avenue
+    Fleet Street/Indiana Avenue
+    Trafalgar Square/Illinois Avenue 
+
+Yellow
+
+    Leicester Square/Atlantic Avenue
+    Coventry Street/Ventnor Avenue
+    Piccadilly/Marvin Gardens 
+
+Green
+
+    Regent Street/Pacific Avenue
+    Oxford Street/North Carolina Avenue
+    Bond Street/Pennsylvania Avenue 
+
+Dark Blue
+
+    Park Lane/Park Place
+    Mayfair/Boardwalk 
+
+Stations
+
+    King's Cross Station/Reading Railroad
+    Marylebone Station/Pennsylvania Railroad
+    Fenchurch St Station/B. & O. Railroad
+    Liverpool Street Station/Short Line 
+
+Utilities
+
+    Electric Company
+    Water Works 
+'''
+
 class Game:
     '''Represents the game being played'''
     def __init__(self, gs, cntPlayers,):
-        if gs != GameStyle.monopoly:
+        if gs not in (GameStyle.monopolyuk, ):
             raise NotImplementedError("Only Monopoloy is supported currently")
-        if cntPlayers > len(MonopolyPieceStyle):
-            raise NotImplementedError("Two many players for the number of available pieces")
+        else:
+            self._gameStyle = gs
+            if cntPlayers > len(MonopolyPieceStyle):
+                raise NotImplementedError("Two many players for the number of available pieces")
+            else:
+                self._playerCount = cntPlayers
+            self.__initializeMonopolyStyleGame()
+
+    def __initializeMonopolyStyleGame(self):
+        pass
 
 
 class Board:
@@ -52,8 +177,9 @@ class Square(Place):
     where positions are 'squares'
     (such as Monopoly)
     '''
-    def __init__(self):
-        pass
+    def __init__(self, name, sqstyle, propertystyle, sqside ):
+        self.name = name 
+        self.name = name 
 
 
 class Piece():
