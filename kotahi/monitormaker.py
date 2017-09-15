c964212b131122c541c581169ecb06298c86fbed
Richard Shea <rshea@thecubagroup.com>
2017-09-16 00:25:44 +1200

    Changes to monitormaker to make rudimentary board. Needs work on make the squares fit together better

--- kotahi/monitormaker.py
+++ kotahi/monitormaker.py
@@ -14,7 +14,11 @@ import argparse
 import glob
 import jsonpickle
 
-OUTPUTFILENAME = "gamestate.js"
+from pybars import Compiler
+
+OUTPUT_JS_FILENAME = "gamestate.js"
+OUTPUT_HTML_FILENAME = "gamestate.html"
+HTML_TEMPLATE_FILENAME = "gamestate.hbr"
 
 class Error(Exception):
     """Base class for exceptions in this module."""
@@ -36,34 +40,67 @@ def getargs():
     parser = argparse.ArgumentParser(description='Parse directory of board game state files and outputs a JS data structure equivalent.')
     parser.add_argument('-i','--inputdir', help='Directory containing the input files', required=True)
     parser.add_argument('-o','--outputdir', help='Directory into which the output files should be written.', required=True)
-    parser.add_argument('-t','--template', help='Path to template file which forms basis of output.', required=True)
+    parser.add_argument('-t','--template', help='Directory containing the gamestate.hbr template basis of the html output.', required=True)
     args = vars(parser.parse_args())
 
     return args
 
 
-def makeoutputpath(args):
-    return os.path.join(args['outputdir'], OUTPUTFILENAME)
+def make_path_to_js_output(args):
+    return os.path.join(args['outputdir'], OUTPUT_JS_FILENAME)
+
+def make_path_to_html_output(args):
+    return os.path.join(args['outputdir'], OUTPUT_HTML_FILENAME)
+
+def make_path_to_html_template(args):
+    return os.path.join(args['template'], HTML_TEMPLATE_FILENAME)
 
 
 def validateargs(args):
+    #Input dir exists ?
     if os.path.isdir(args['inputdir']) == False:
         raise PathError("The inputdir path does not exist")
+    #Output dir exists ?
     if os.path.isdir(args['outputdir']) == False:
         raise PathError("The outputdir path does not exist")
-    if os.path.exists(makeoutputpath(args)):
-        raise PathError("The output file already exists")
-    if os.path.exists(args['template']) == False:
+    #JS output file already exists ?        
+    if os.path.exists(make_path_to_js_output(args)):
+        raise PathError("The js output file already exists")
+    #HTML output file already exists ?        
+    if os.path.exists(make_path_to_html_output(args)):
+        raise PathError("The html output file already exists")
+    #HTML template exists ?        
+    if os.path.exists(make_path_to_html_template(args)) == False:
         raise PathError("The template file does not exist")
 
 
-def processfiles(outputpath, inputdir):
+def processfiles(   path_to_output_js, 
+                    path_to_output_html, 
+                    path_to_html_template, 
+                    inputdir):
 
     strout = ""
     lstpath = glob.glob(os.path.join(inputdir, '*'))
     lstpath.sort()
 
-    with open(outputpath, 'w', encoding='utf-8') as fout:
+    #Build HTML file corresponding to game squares etc
+    lstout = []
+    for idxrows in range(12):
+        lstout.append('''<div class='row'>''')
+        for idxcols in range(12):
+            lstout.append('''<div class='col-md-1'><div class='demo-content'>{0}/{1}</div></div>'''.format(idxcols + 1, idxrows + 1))
+        lstout.append('''</div>''')
+    strout = ''.join(lstout)
+
+    #Build HTML file corresponding to game squares etc
+    with open(path_to_html_template, 'r', encoding='utf-8') as ftemplate:
+        template = ftemplate.read()
+        with open(path_to_output_html, 'w', encoding='utf-8') as fhtmlout:
+            fhtmlout.write(template.format(testrow=strout))
+            
+
+    #Build JS structure
+    with open(path_to_output_js, 'w', encoding='utf-8') as fout:
         fout.write('o={}\n')
         for p in lstpath:
             with open(p, 'r', encoding='utf-8') as fin:
@@ -74,7 +111,10 @@ def main():
     dargs = getargs()
     validateargs(dargs)
     pprint.pprint(dargs)
-    processfiles(makeoutputpath(dargs), dargs['inputdir'])
+    processfiles(   make_path_to_js_output(dargs), 
+                    make_path_to_html_output(dargs),
+                    make_path_to_html_template(dargs),
+                    dargs['inputdir'])
 
 
 if __name__ == "__main__":
